8d9d70050899ec10c6ed7ec769247b9635504979
Sebastian <seba7964@gmail.com>
2020-02-23 23:40:46 +0100

    finished api for creating data

--- dezi-clean/Controllers/FileUploadingController.cs
+++ dezi-clean/Controllers/FileUploadingController.cs
@@ -16,28 +16,28 @@ namespace dezi_clean.Controllers
     {
         [HttpPost]
         [Route("api/FileUploading/UploadFile")]
-        public async Task <string> UploadFile() {
+        public async Task<string> UploadFile()
+        {
 
             var ctx = HttpContext.Current;
             var root = ctx.Server.MapPath("~/slike");
 
             var provider =
-                new MultipartFormDataStreamProvider (root);
-            
+                new MultipartFormDataStreamProvider(root);
+
             try
             {
-               await Request.Content.ReadAsMultipartAsync(provider);
+                await Request.Content.ReadAsMultipartAsync(provider);
 
-                var testValue = provider.FormData.GetValues("namee")[0];
                 string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
                 SqlConnection con = new SqlConnection(connStr);
                 con.Open();
-                {  
-                    foreach (var file in provider.FileData)
                 {
+                    foreach (var file in provider.FileData)
+                    {
 
 
-                    var name = file.Headers.ContentDisposition.FileName;
+                        var name = file.Headers.ContentDisposition.FileName;
                         //Remove dobule quotes from string
                         name = name.Trim('"');
                         var localFileName = file.LocalFileName;
@@ -45,27 +45,30 @@ namespace dezi_clean.Controllers
 
 
 
-                    File.Move(localFileName, filePath);
-                    
+                        File.Move(localFileName, filePath);
 
-                    var namee = provider.FormData.GetValues("namee")[0];
-                    var lastname = provider.FormData.GetValues("lastname")[0];
-                    var latitude = provider.FormData.GetValues("latitude")[0];
-                    var longitude = provider.FormData.GetValues("longitude")[0];
+
+                        var namee = provider.FormData.GetValues("ime")[0];
+                        var lastname = provider.FormData.GetValues("prezime")[0];
+                        var opisproblema = provider.FormData.GetValues("opis")[0];
+                        var latitude = provider.FormData.GetValues("latitude")[0];
+                        var longitude = provider.FormData.GetValues("longitude")[0];
+                        var aktivan = "true";
 
 
                         SqlCommand cmd = con.CreateCommand();
-                        //cmd.CommandText = "INSERT INTO [dezi-me].dbo.Podaci (name, lastname, latitude, longitude,imagepath) VALUES( @namee, @lastname,@latitude, @longitude, @imagepath)";
-                        cmd.CommandText = "INSERT INTO [dezi-me].dbo.Podaci VALUES( @name, @lastname,@latitude, @longitude, @imagepath)";
+                        cmd.CommandText = "INSERT INTO [dezi-me].dbo.Data VALUES( @name, @lastname,@problemdescription,@latitude, @longitude, @imagepath, @aktivan)";
                         cmd.Parameters.AddWithValue("@name", namee);
                         cmd.Parameters.AddWithValue("@lastname", lastname);
+                        cmd.Parameters.AddWithValue("@problemdescription", opisproblema);
                         cmd.Parameters.AddWithValue("@latitude", latitude);
                         cmd.Parameters.AddWithValue("@longitude", longitude);
                         cmd.Parameters.AddWithValue("@imagepath", filePath);
+                        cmd.Parameters.AddWithValue("@aktivan", aktivan);
                         cmd.ExecuteNonQuery();
                         con.Close();
                     }
-                    
+
                 }
             }
             catch (Exception e)
@@ -73,7 +76,7 @@ namespace dezi_clean.Controllers
                 return $"Error: {e.Message}";
             }
             return "File uploaded!";
-        
-    }
+
+        }
     }
 }
